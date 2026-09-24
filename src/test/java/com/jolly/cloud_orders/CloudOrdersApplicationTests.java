572e2f61f7dd949c3845b7a149ada5c746457875
jolly <ktaranau@gmail.com>
2026-09-24 19:36:10 +0200

    Restore minimum amount test

--- src/test/java/com/jolly/cloud_orders/CloudOrdersApplicationTests.java
+++ src/test/java/com/jolly/cloud_orders/CloudOrdersApplicationTests.java
@@ -47,7 +47,7 @@ void acceptsMinimumAmount() throws Exception {
     mockMvc.perform(post("/orders")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content("""
-                        {"customerEmail":"email@mail.com","totalAmount":0.02}
+                        {"customerEmail":"email@mail.com","totalAmount":0.01}
                         """))
             .andExpect(status().isCreated())
             .andExpect(jsonPath("$.id").isNotEmpty())
